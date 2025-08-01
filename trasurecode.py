6642cfe27fcf086e597c05ee29c094e3d0280dd9
Rajusharma1095 <rajusharma91238@gmail.com>
2025-08-01 12:04:56 +0000

    pypypy

---
+++ trasurecode.py
@@ -0,0 +1,21 @@
+print("Welcome to Treasure Island.  ")
+print("Your mission is to find the treasure")
+print("You are at a cross road. where do you want to go?")
+
+rode=input('Type "left" or "right" :')
+if rode=="left":
+    print("You have come to lake. There is an island in the middle of the lake.")
+    river=input('Type "wait" to wait for the boat. Type "swim" to swim across')
+    if river=="wait":
+        print("You are arived at the island unharmed. There is a house with 3 doors. ")
+        island=input('One red, one yellow and one blue. Which colour do you choose? ')
+        if island=="yellow" :
+            print("You win!")
+        elif island=="red"or island=="blue":
+            print("Game Over.")
+    elif river=="swim":
+        print("Game over.")
+elif rode=="right":
+    print("Game over.")le
+
+      
